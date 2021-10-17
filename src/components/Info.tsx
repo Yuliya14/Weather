8b8e310478aa867f7957cc58359a8cc26670c3c7
Yuliya <julia1997.04.14@mail.ru>
2021-10-17 21:11:47 +0300

    Cganhe CSS: color placeholder and with container

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Info = () => {
     return (
         <div className={"info"}>
-            <h2>Погодное приложение</h2>
-            <p>Узнайте погоду в вашем городе!</p>
+            <h2>Weather</h2>
+            <p>Find out the weather in your city!</p>
         </div>
     )
 }
